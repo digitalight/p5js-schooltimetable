6968bf3126c038c777883200d0c6610560105ecb
digitalight <mikeglover06@gmail.com>
2017-04-01 18:27:50 +0100

    Move to JSON file and redesign

--- sketch.js
+++ sketch.js
@@ -1,7 +1,58 @@
+var lessonData;
+var lessonsToday = [];
+var lessonStart;
+var lessonEnd;
+var isLesson = false;
+var percentage;
+
+function preload() {
+//setTimeout(function(){window.open("/", "School Timetable", "width=480,height=95,titlebar=no,menubar=no,toolbar=no,scrollbars=no,resizeable=no,status=no,location=no");}, 1);
+    lessonData = loadJSON("data/lessons.json");
+
+
+
+}
+
 function setup() {
-    noCanvas();
+    noCanvas(); // Needed to stop canvas being made
+    getTodayLessons();
+    checkLesson();
+    setInterval(checkLesson, 1000);
     timeIt();
     setInterval(timeIt, 1000);
+
+    // Go through JSON and get todays lessons objects and put in lessonToday array
+    function getTodayLessons() {
+        var lessons = lessonData.lesson; // Lesson objects in JSON
+        var today = dayofweek(); // Get name of today
+        for (var i = 0; i < lessons.length; i++) { // Loop through every lesson object
+            var days = lessons[i].days;
+            for (var j = 0; j < days.length; j++) { // Loop through each Day array that is in the lesson object
+                if (days[j] == today) { // Filter just today in Day array
+                    lessonsToday.push(lessons[i]); // Add lesson object into a new Array that contains just Todays lessons.
+                }
+            }
+        }
+    }
+
+    // Go through lessonsToday array, get current time and then check if its between any lesson start and end time.
+    function checkLesson() {
+        var d = new Date();
+        var n = d.getTime();
+        currentLesson = select("#currentLesson");
+        for (var i = 0; i < lessonsToday.length; i++) {
+            if ((n > calcTime(lessonsToday[i].start)) && (n < calcTime(lessonsToday[i].end))) {
+              currentLesson.html(lessonsToday[i].name);
+              lessonStart = lessonsToday[i].start;
+              lessonEnd = lessonsToday[i].end;
+              isLesson = true;
+              //  console.log(lessonsToday[i].name);
+            } else {
+              currentLesson.html("No Lesson");
+              isLesson = false;
+            }
+        }
+    }
 
     function timeIt() {
         var curtime = select('#currentTime');
@@ -13,7 +64,13 @@ function setup() {
         d = dayofweek();
         curtime.html(h + ":" + m + ":" + s);
         curday.html(d);
-        currentLesson();
+
+          if (isLesson == true) {
+            select("#remaining").html(timeRemaining(lessonEnd,lessonStart)[0]);
+            select("#remain-progress").style("width", timeRemaining(lessonEnd,lessonStart)[1] + "%")
+          } else {
+            select("#remaining").html("");
+          }
     }
 
     function calcTime(time) {
@@ -26,22 +83,31 @@ function setup() {
         var time3 = time2.getTime();
         return time3;
     }
+    // Convert String Time in JSON to Unix time format
+    function unixTime(strTime) {
+      var mon = month();
+      var today = day();
+      var thisyear = year();
+      var timearray = [mon, today, thisyear, strTime];
+      var time = new Date(join(timearray, " ")).getTime();
+      return time;
+    }
 
-    function timeRemaining(time) {
-        var mon = month();
-        var today = day();
-        var thisyear = year();
-        var timearray = [mon, today, thisyear, time];
-        var time1 = join(timearray, " ");
-        var time2 = new Date(time1);
-        var diff = new Date(time2.getTime() - (new Date()));
+    function timeRemaining(endTime,startTime) {
+        var start = unixTime(startTime);
+        var end = unixTime(endTime);
+        var current = new Date().getTime();
+        var diff = new Date(end - current);
+        var prog = new Date(current - start).getTime();
+        var percentage = floor(prog / (prog + diff.getTime()) * 100);
         var diffhours = diff.getHours();
         var diffmin = diff.getMinutes();
         var diffsec = diff.getSeconds();
         var remain = diffhours + ":" + nf(diffmin, 2) + ":" + nf(diffsec, 2);
-        return remain;
+        return [remain, percentage];
     }
 
+    // Function to get current day of the week.
     function dayofweek() {
         var d = new Date();
         var weekday = new Array(7);
@@ -54,53 +120,5 @@ function setup() {
         weekday[6] = "Saturday";
         var n = weekday[d.getDay()];
         return n;
-    }
-
-    function currentLesson() {
-        var d = new Date();
-        var n = d.getTime();
-        var l01 = select('#l01');
-        var l02 = select('#l02');
-        var lb = select('#lb');
-        var l03 = select('#l03');
-        var l04 = select('#l04');
-        var lass = select('#lass');
-        var lun = select('#lun');
-        var lreg = select('#lreg');
-        var l05 = select('#l05');
-        var l06 = select('#l06');
-
-        if ((n > calcTime("08:55")) && (n < calcTime("09:45"))) {
-          l01.class('list-group-item list-group-item-success');
-        } else if ((n > calcTime("09:45")) && (n < calcTime("10:35"))) {
-          l01.class('list-group-item');
-          l02.class('list-group-item list-group-item-success');
-        } else if ((n > calcTime("10:35")) && (n < calcTime("10:55"))) {
-          l02.class('list-group-item');
-          lb.class('list-group-item list-group-item-success');
-        } else if ((n > calcTime("10:55")) && (n < calcTime("11:45"))) {
-          lb.class('list-group-item');
-          l03.class('list-group-item list-group-item-success');
-        } else if ((n > calcTime("11:45")) && (n < calcTime("12:35"))) {
-          l03.class('list-group-item');
-          l04.class('list-group-item list-group-item-success');
-        } else if ((n > calcTime("12:35")) && (n < calcTime("12:55"))) {
-          l04.class('list-group-item');
-          lass.class('list-group-item list-group-item-success');
-        } else if ((n > calcTime("12:55")) && (n < calcTime("13:40"))) {
-          lass.class('list-group-item');
-          lun.class('list-group-item list-group-item-success');
-        } else if ((n > calcTime("13:40")) && (n < calcTime("13:50"))) {
-          lun.class('list-group-item');
-          lreg.class('list-group-item list-group-item-success');
-        } else if ((n > calcTime("13:50")) && (n < calcTime("14:35"))) {
-          lreg.class('list-group-item');
-          l05.class('list-group-item list-group-item-success');
-        } else if ((n > calcTime("14:35")) && (n < calcTime("15:30"))) {
-          l05.class('list-group-item');
-          l06.class('list-group-item list-group-item-success');
-        } else {
-            return 0;
-        }
     }
 }
